5c0b11b0bf4d1b6dd0966a58dc62eedb06ff5513
rodrigo <rodrigoandre777@gmail.com>
2020-10-21 09:53:14 -0400

    fix(functions): add legible functions to code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from decimal import Decimal
+
 
 def to_double(object):
     string = object.replace("R$", "")
@@ -8,40 +8,50 @@ def to_double(object):
     return double
 
 
+def rank_lojas(string):
+    loja = data_compressed.loc[string]
+    rank_loja = loja.dropna()
+    return rank_loja
+
+
+def nome_melhor_vendedor(object):
+    nome = object.sort_values().index[-1]
+    return nome[1]
+
+
+def total_vendas_melhor_vendedor(object):
+    return object.sort_values().values[-1]
+
+
+def melhor_da_filial(object):
+    return (nome_melhor_vendedor(object), total_vendas_melhor_vendedor(object))
+
+
+
 data = pd.read_csv("data/data.csv")
 
 data = data[['Filial', 'nome', 'valor_compra']]
 data["valor_compra"] = data["valor_compra"].map(to_double)
 data_compressed = data.pivot_table(index='Filial', columns='nome', aggfunc=sum)
 
-manoa = data_compressed.loc['Loja Manoa']
-manoa = manoa.dropna()
+data_manoa = rank_lojas('Loja Manoa')
+data_armando_mendes = rank_lojas('Loja Armando Mendes')
+data_cachoerinha = rank_lojas('Loja Cachoerinha')
 
-armando_mendes = data_compressed.loc['Loja Armando Mendes']
-armando_mendes = armando_mendes.dropna()
+melhor_manoa = melhor_da_filial(data_manoa)
+melhor_armando = melhor_da_filial(data_armando_mendes)
+melhor_cachoerinha = melhor_da_filial(data_cachoerinha)
 
-cachoerinha = data_compressed.loc['Loja Cachoerinha']
-cachoerinha = cachoerinha.dropna()
-
-nome_manoa = manoa.sort_values().index[-1]
-nome_armando = armando_mendes.sort_values().index[-1]
-nome_cachoerinha = cachoerinha.sort_values().index[-1]
-
-melhor_manoa = (nome_manoa[1], manoa.sort_values().values[-1])
-melhor_armando = (nome_armando[1] ,  armando_mendes.sort_values().values[-1])
-melhor_cachoerinha = (nome_cachoerinha[1] ,  cachoerinha.sort_values().values[-1])
-
-# print(melhor_manoa, melhor_armando, melhor_cachoerinha)
 
 melhores_vendedores = pd.DataFrame({
-    'loja': ['Manoa', 'Armando Mendes', 'Cachoerinha'],
     'nome':[melhor_manoa[0], melhor_armando[0], melhor_cachoerinha[0]],
     'valor total': [melhor_manoa[1], melhor_armando[1], melhor_cachoerinha[1]]
-    })
+    }, index=['Loja Manoa', 'Loja Armando Mendes', 'Loja Cachoerinha'])
 
 print(melhores_vendedores)
 
+#erro ao plotar corretamente
 # data_mv = melhores_vendedores.sort_values(by="valor total")
-# plot = data_mv[['valor total', 'nome']].plot(kind='bar')
+# plot = data_mv['valor total'].plot(kind='bar', ylabel="Total de vendas em R$", legend=True)
 # fig = plot.get_figure()
 # fig.savefig('output_1.png')
