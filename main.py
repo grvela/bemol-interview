7e89f7c797fef783e34264ba78a473def4efe413
rodrigo <rodrigoandre777@gmail.com>
2020-10-20 20:34:15 -0400

    feat(plot/export): plot and exporting dataframes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from decimal import Decimal
 
 def to_double(object):
     string = object.replace("R$", "")
@@ -8,23 +8,40 @@ def to_double(object):
     return double
 
 
-
 data = pd.read_csv("data/data.csv")
 
 data = data[['Filial', 'nome', 'valor_compra']]
-
 data["valor_compra"] = data["valor_compra"].map(to_double)
-
-print(data)
-
 data_compressed = data.pivot_table(index='Filial', columns='nome', aggfunc=sum)
 
-print(data_compressed)
-
 manoa = data_compressed.loc['Loja Manoa']
-
-print(manoa)
-
 manoa = manoa.dropna()
 
-print(manoa.sort_values()) #esse aqui é o que mais vendeu
+armando_mendes = data_compressed.loc['Loja Armando Mendes']
+armando_mendes = armando_mendes.dropna()
+
+cachoerinha = data_compressed.loc['Loja Cachoerinha']
+cachoerinha = cachoerinha.dropna()
+
+nome_manoa = manoa.sort_values().index[-1]
+nome_armando = armando_mendes.sort_values().index[-1]
+nome_cachoerinha = cachoerinha.sort_values().index[-1]
+
+melhor_manoa = (nome_manoa[1], manoa.sort_values().values[-1])
+melhor_armando = (nome_armando[1] ,  armando_mendes.sort_values().values[-1])
+melhor_cachoerinha = (nome_cachoerinha[1] ,  cachoerinha.sort_values().values[-1])
+
+# print(melhor_manoa, melhor_armando, melhor_cachoerinha)
+
+melhores_vendedores = pd.DataFrame({
+    'loja': ['Manoa', 'Armando Mendes', 'Cachoerinha'],
+    'nome':[melhor_manoa[0], melhor_armando[0], melhor_cachoerinha[0]],
+    'valor total': [melhor_manoa[1], melhor_armando[1], melhor_cachoerinha[1]]
+    })
+
+print(melhores_vendedores)
+
+# data_mv = melhores_vendedores.sort_values(by="valor total")
+# plot = data_mv[['valor total', 'nome']].plot(kind='bar')
+# fig = plot.get_figure()
+# fig.savefig('output_1.png')
